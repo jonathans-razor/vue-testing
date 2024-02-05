933d1ec1deb11669bbf66d49d58c08239c7f72dc
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-05 15:42:16 -0500

    Feb-5-2024-3-42-16-PM

--- vt1/src/App.vue
+++ vt1/src/App.vue
@@ -1,11 +1,35 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 
-const counter = reactive({ count: 0 })
-const message = ref('Hello World!')
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
-  <p>Count is: {{ counter.count }}</p>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
