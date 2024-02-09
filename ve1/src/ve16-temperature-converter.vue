d6747ad550697bc7577257e6aa93693728fabc5b
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-09 14:45:20 -0500

    Feb-9-2024-2-45-20-PM

---
+++ ve1/src/ve16-temperature-converter.vue
@@ -0,0 +1,25 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks/#temp
+-->
+
+<script setup>
+import { ref } from 'vue'
+
+const celsius = ref(0)
+const fahrenheit = ref(32)
+
+function setCelsius(eventObject, targetValue = +eventObject.target.value) {
+  celsius.value = targetValue
+  fahrenheit.value = targetValue * (9 / 5) + 32
+}
+
+function setFahrenheit(eventObject, targetValue = +eventObject.target.value) {
+  fahrenheit.value = targetValue
+  celsius.value = (targetValue - 32) * (5 / 9)
+}
+</script>
+
+<template>
+  <input type="number" :value="celsius" @change="setCelsius"> Celsius =
+  <input type="number" :value="fahrenheit" @change="setFahrenheit"> Fahrenheit
+</template>
