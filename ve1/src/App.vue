76664e1860545b86aab1c3e68af5b614bef49f45
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-09 18:15:44 -0500

    Feb-9-2024-6-15-44-PM

--- ve1/src/App.vue
+++ ve1/src/App.vue
@@ -1,142 +1,25 @@
 <!--
-https://eugenkiss.github.io/7guis/tasks/#circle
+An example of creating a reusable grid component and using it with external data.
 -->
 
 <script setup>
-import { ref, shallowReactive, toRaw } from 'vue'
+import DemoGrid from './Grid.vue'
+import { ref } from 'vue'
 
-/*
-The argument passed to shallowReactive is [[]], which is an array containing an empty array. This means that history is initialized as a reactive object that contains a single element, which is an empty array.
-*/
-const history = shallowReactive([[]])
-const index = ref(0)
-const circles = ref([])
-const selected = ref()
-const adjusting = ref(false)
-
-function onClick({ clientX: x, clientY: y }) {
-  if (adjusting.value) {
-    adjusting.value = false
-    selected.value = null
-    push()
-    return
-  }
-
-  selected.value = [...circles.value].reverse().find(({ cx, cy, r }) => {
-    const dx = cx - x
-    const dy = cy - y
-    return Math.sqrt(dx * dx + dy * dy) <= r
-  })
-
-  if (!selected.value) {
-    circles.value.push({
-      cx: x,
-      cy: y,
-      r: 50
-    })
-    push()
-  }
-}
-
-function adjust(circle) {
-  selected.value = circle
-  adjusting.value = true
-}
-
-function push() {
-  history.length = ++index.value
-  history.push(clone(circles.value))
-  console.log(toRaw(history))
-}
-
-function undo() {
-  circles.value = clone(history[--index.value])
-}
-
-function redo() {
-  circles.value = clone(history[++index.value])
-}
-
-function clone(circles) {
-  return circles.map((c) => ({ ...c }))
-}
+const searchQuery = ref('')
+const gridColumns = ['name', 'power']
+const gridData = [
+  { name: 'Chuck Norris', power: Infinity },
+  { name: 'Bruce Lee', power: 9000 },
+  { name: 'Jackie Chan', power: 7000 },
+  { name: 'Jet Li', power: 8000 }
+]
 </script>
 
 <template>
-  <svg @click="onClick">
-    <foreignObject x="0" y="40%" width="100%" height="200">
-      <p class="tip">
-        Click on the canvas to draw a circle. Click on a circle to select it.
-        Right-click on the canvas to adjust the radius of the selected circle.
-      </p>
-    </foreignObject>
-    <circle v-for="circle in circles" :cx="circle.cx" :cy="circle.cy" :r="circle.r"
-      :fill="circle === selected ? '#ccc' : '#fff'" @click="selected = circle" @contextmenu.prevent="adjust(circle)">
-    </circle>
-  </svg>
-
-  <div class="controls">
-    <button @click="undo" :disabled="index <= 0">Undo</button>
-    <button @click="redo" :disabled="index >= history.length - 1">Redo</button>
-  </div>
-
-  <div class="dialog" v-if="adjusting" @click.stop>
-    <p>Adjust radius of circle at ({{ selected.cx }}, {{ selected.cy }})</p>
-    <input type="range" v-model="selected.r" min="1" max="300">
-  </div>
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <DemoGrid :data="gridData" :columns="gridColumns" :filter-key="searchQuery">
+  </DemoGrid>
 </template>
-
-<style>
-body {
-  margin: 0;
-  overflow: hidden;
-}
-
-svg {
-  width: 100vw;
-  height: 100vh;
-  background-color: #cb49ab;
-}
-
-circle {
-  stroke: #000;
-}
-
-.controls {
-  position: fixed;
-  top: 10px;
-  left: 0;
-  right: 0;
-  text-align: center;
-}
-
-.controls button+button {
-  margin-left: 6px;
-}
-
-.dialog {
-  position: fixed;
-  top: calc(50% - 50px);
-  left: calc(50% - 175px);
-  background: #ad4242;
-  width: 350px;
-  height: 100px;
-  padding: 5px 20px;
-  box-sizing: border-box;
-  border-radius: 4px;
-  text-align: center;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
-}
-
-.dialog input {
-  display: block;
-  width: 200px;
-  margin: 0px auto;
-}
-
-.tip {
-  text-align: center;
-  padding: 0 50px;
-  color: #378328;
-}
-</style>
