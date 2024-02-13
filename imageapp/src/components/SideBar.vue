48ee69de64ab180b89a30794fa509533306a92e1
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-13 17:29:15 -0500

    Feb-13-2024-5-29-14-PM

--- imageapp/src/components/SideBar.vue
+++ imageapp/src/components/SideBar.vue
@@ -2,7 +2,7 @@
   <v-card>
     <v-layout>
       <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
-        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/86.jpg" title="Jonathan" nav>
           <template v-slot:append>
             <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
           </template>
@@ -11,12 +11,12 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-image" title="Color" value="home"></v-list-item>
           <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
           <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
         </v-list>
       </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
+      <v-main style="height: 220px"></v-main>
     </v-layout>
   </v-card>
 </template>
