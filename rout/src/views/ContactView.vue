c67d895413eba5b824c268249c407fa9e8776ff7
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-15 11:53:45 -0500

    Feb-15-2024-11-53-45-AM

---
+++ rout/src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+    <HelloWorld msg="Welcome to your contact page Jonathan."/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'ContactView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
