16b73f8b19f7b0ce6d775e6d2b5eabfde4b2948f
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-22 16:27:39 -0500

    Feb-22-2024-4-27-39-PM

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -43,11 +43,32 @@ onMounted(() => {
       <h3>CREATE A TODO</h3>
       <form @submit.prevent="addTodo">
         <h4>What's on your todo list?</h4>
-      </form>
         <input 
-          type="text" 
-          placeholder="e.g. Make a video" 
-          v-model="input_content"/>
+        type="text" 
+        placeholder="e.g. Make a video" 
+        v-model="input_content"/>
+        <h4>Pick a category</h4>
+        <div class="options">
+            <label>
+              <input 
+              type="radio" 
+              name="category"
+              value="business" 
+              v-model="input_category"/>
+              <span class="bubble business"></span>
+              <div>Business</div>
+            </label>
+            <label>
+              <input 
+              type="radio" 
+              name="category"
+              value="personal" 
+              v-model="input_category"/>
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+            </label>
+          </div>
+      </form>
     </section>
   </main>
 
