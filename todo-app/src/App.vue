48a9e5c31e6e37c08e52aa35d4e7b95ba90071f7
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-22 16:17:50 -0500

    Feb-22-2024-4-17-50-PM

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -11,6 +11,16 @@ const tudos_asc = computed(() => tudos.value.sort((a, b) => {
   return a.createdAt = b.createdAt
 }))
 
+const addTodo = () => {
+  tudos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    createdAt: new Date()
+  })
+  input_content.value = ''
+  input_category.value = null
+}
+
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
 })
@@ -27,6 +37,16 @@ onMounted(() => {
       <h2 class="title">
         What's up, <input type="text" placeholder="Name here" v-model="name"/>
       </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+      </form>
+        <input type="text" placeholder="e.g. Make a video" 
+          v-model="input_content"/>
+        {{ input_content }}
     </section>
   </main>
 
