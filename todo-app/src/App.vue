f4edca643ee257446f43b50d291e3c0fed07052b
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-22 15:22:52 -0500

    Feb-22-2024-3-22-52-PM

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
+import { ref, onMounted, computed, watch } from 'vue'
 
+const tudos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category= ref(null)
+
+const tudos_asc = computed(() => tudos.value.sort((a, b) => {
+  return a.createdAt = b.createdAt
+}))
+
+watch(name, (newName) => {
+  localStorage.setItem('name', newVal)
+})
 </script>
 
 <template>
+
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name"/>
+      </h2>
+    </section>
+  </main>
 
 </template>
