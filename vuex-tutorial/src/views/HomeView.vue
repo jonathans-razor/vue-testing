ec6cd612420a77ec216c424eb610c1247da0ef4e
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-26 13:08:25 -0500

    Feb-26-2024-1-08-25-PM

--- vuex-tutorial/src/views/HomeView.vue
+++ vuex-tutorial/src/views/HomeView.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div class="counter">
+      0
+    </div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'HomeView'
 }
+
 </script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+  text-align: center;
+}
+</style>
