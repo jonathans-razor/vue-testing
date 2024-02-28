c5616b55d103dd7e2b5aa76e577aaace8158be9d
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-28 12:25:13 -0500

    Feb-28-2024-12-25-12-PM

--- vuetify-app-feb-27-2024/src/App.vue
+++ vuetify-app-feb-27-2024/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <!--  -->
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      :width="146">
+      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link title="List Item 1"></v-list-item>
+      <v-list-item link title="List Item 2"></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
     </v-navigation-drawer>
-
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
