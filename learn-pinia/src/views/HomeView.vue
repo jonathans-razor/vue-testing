f9bed52837e3cac45b1b0a50306b2c29ba0a7405
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-01 13:49:11 -0500

    Mar-1-2024-1-49-10-PM

--- learn-pinia/src/views/HomeView.vue
+++ learn-pinia/src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <template>
 <div class="home">
-<div class="count">
-  0
-</div>
+  <div class="count">
+    {{ count }}
+  </div>
+  <div class="buttons">
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button> 
+  </div>
 </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const increment = () => count.value++
+const decrement = () => count.value--
+const count = ref(0)
+</script>
 
 <style>
 .count {
   font-size: 60px;
   margin: 20px;
 }
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+} 
 </style>
