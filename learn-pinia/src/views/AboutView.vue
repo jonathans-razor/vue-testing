08e9ec9dd9ea7f9c435bd468edd5c9d14b5b6afb
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-01 16:26:52 -0500

    Mar-1-2024-4-26-52-PM

--- learn-pinia/src/views/AboutView.vue
+++ learn-pinia/src/views/AboutView.vue
@@ -3,5 +3,21 @@
     <h1>
       About
     </h1>
+    <button 
+      @click="storeCounter.increment"
+      class="counter-button">
+      {{ storeCounter.count }}
+    </button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const storeCounter = useCounterStore()
+</script>
+
+<style>
+.counter-button {
+  font-size: 50px;
+}
+</style>
