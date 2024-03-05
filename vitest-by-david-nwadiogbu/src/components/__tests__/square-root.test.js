ca8ecd1a3e8d20dcb37d2f9c555f3da595388937
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-05 16:59:23 -0500

    Mar-5-2024-4-59-23-PM

--- vitest-by-david-nwadiogbu/src/components/__tests__/square-root.test.js
+++ vitest-by-david-nwadiogbu/src/components/__tests__/square-root.test.js
@@ -5,3 +5,15 @@ describe('Square root', () => {
     expect(Math.sqrt(4)).toBe(2);
   });
 }); // Add closing parenthesis here
+
+describe('Square root 2', () => {
+  it("finds the square root", () => {
+    expect(Math.sqrt(49)).toBe(7);
+  });
+}); // Add closing parenthesis here
+
+describe('Square root 3', () => {
+  it("finds the square root", () => {
+    expect(Math.sqrt(64)).toBe(8);
+  });
+}); // Add closing parenthesis here
