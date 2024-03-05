de69205248d2009348fc4e54d09d025eeb48a9ff
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-05 16:57:36 -0500

    Mar-5-2024-4-57-35-PM

---
+++ vitest-by-david-nwadiogbu/src/components/__tests__/square-root.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Square root', () => {
+  it("finds the square root", () => {
+    expect(Math.sqrt(4)).toBe(2);
+  });
+}); // Add closing parenthesis here
