d01720477bab3256fb344c916f2fb8c0f37adf27
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-14 14:17:22 -0400

    Mar-14-2024-2-17-22-PM

--- ve1/src/App.vue
+++ ve1/src/App.vue
@@ -1,52 +1,86 @@
 <!--
-This example demonstrates handling user input with the v-on directive.
+An SVG graph
 -->
 
 <script setup>
-import { ref } from 'vue'
+import PolyGraph from './PolyGraph.vue'
+import { ref, reactive } from 'vue'
 
-const message = ref('Yello world.')
+const newLabel = ref('')
+const stats = reactive([
+  { label: 'A', value: 100 },
+  { label: 'B', value: 100 },
+  { label: 'C', value: 100 },
+  { label: 'D', value: 100 },
+  { label: 'E', value: 100 },
+  { label: 'F', value: 100 }
+])
 
-function reverseMessage() {
-  // Access/mutate the value of a ref via
-  // its .value property.
-  message.value = message.value.split('').reverse().join('')
+function add(e) {
+  e.preventDefault()
+  if (!newLabel.value) return
+  stats.push({
+    label: newLabel.value,
+    value: 100
+  })
+  newLabel.value = ''
 }
 
-function notify() {
-  alert('navigation was prevented.')
+function remove(stat) {
+  if (stats.length > 3) {
+    stats.splice(stats.indexOf(stat), 1)
+  } else {
+    alert("Can't delete more!")
+  }
 }
 </script>
 
 <template>
-  <!--
-    Note we don't need .value inside templates because
-    refs are automatically "unwrapped" in templates.
-  -->
-  <h1>{{ message }}</h1>
+  <svg width="200" height="200">
+    <PolyGraph :stats="stats"></PolyGraph>
+  </svg>
 
-  <!--
-    Bind to a method/function.
-    The @click syntax is short for v-on:click.
-  -->
-  <button @click="reverseMessage">Reverse Message</button>
+  <!-- controls -->
+  <div v-for="stat in stats">
+    <label>{{ stat.label }}</label>
+    <input type="range" v-model="stat.value" min="0" max="100">
+    <span>{{ stat.value }}</span>
+    <button @click="remove(stat)" class="remove">X</button>
+  </div>
 
-  <!-- Can also be an inline expression statement -->
-  <button @click="message += '!!'">Append "!!"</button>
+  <form id="add">
+    <input name="newlabel" v-model="newLabel">
+    <button @click="add">Add a Stat</button>
+  </form>
 
-  <!--
-    Vue also provides modifiers for common tasks
-    such as e.preventDefault() and e.stopPropagation()
-  -->
-  <a href="https://vuejs.org" @click.prevent="notify">
-    A link with e.preventDefault()
-  </a>
+  <pre id="raw">{{ stats }}</pre>
 </template>
 
 <style>
-button,
-a {
-  display: block;
-  margin-bottom: 3em;
+polygon {
+  fill: #42b983;
+  opacity: 0.75;
+}
+
+circle {
+  fill: transparent;
+  stroke: #999;
+}
+
+text {
+  font-size: 10px;
+  fill: #666;
+}
+
+label {
+  display: inline-block;
+  margin-left: 10px;
+  width: 20px;
+}
+
+#raw {
+  position: absolute;
+  top: 0;
+  left: 300px;
 }
 </style>
--- vt1/src/App.vue
+++ vt1/src/App.vue
@@ -1,15 +1,5 @@
-<script setup>
-import { ref } from 'vue'
-
-const awesome = ref(true)
-
-function toggle() {
-  awesome.value = !awesome.value
-}
-</script>
-
 <template>
-  <button @click="toggle">toggle</button>
-  <h1 v-if="awesome">Vue is awesome!</h1>
-  <h1 v-else>Oh no 😢</h1>
+  <h3>
+    Hello world from Jonathan's Vue Tutorials 1-10.
+  </h3>
 </template>
