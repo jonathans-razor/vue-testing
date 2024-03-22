36298f9b9f37fc5cd3e4ff151f86e37353bc68a7
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-22 16:54:41 -0400

    Mar-22-2024-4-54-41-PM

--- caro/src/App.vue
+++ caro/src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue'
+import L from 'leaflet'
 
 const latitude = ref(0);
 const longitude = ref(0);
+const map = ref();
+const mapContainer = ref();
+
+onMounted(() => {
+  map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map.value);
+}); 
 
 function getLocation() {
   console.log("getLocation begin");
@@ -22,12 +33,17 @@ function getLocation() {
 
 
 <template>
-<button @click="getLocation">Get Location</button>
-{{ latitude }}, {{ longitude }}
+  <button @click="getLocation">Get Location</button>
+  {{ latitude }}, {{ longitude }}
+
+  <div ref="mapContainer" style="margin:0px; width: 970px;height: 100vh"></div>
 </template>
 
 
 
 <style scoped>
-
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
