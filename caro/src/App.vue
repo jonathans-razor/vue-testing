22e8094cf510833a7d0c3145960354e21061b2a0
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-22 16:11:03 -0400

    Mar-22-2024-4-11-02-PM

---
+++ caro/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const latitude = ref(0);
+const longitude = ref(0);
+
+function getLocation() {
+  console.log("getLocation begin");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      latitude.value = position.coords.latitude;
+      longitude.value = position.coords.longitude;
+      console.log("Latitude:" + position.coords.latitude);
+      console.log("Longitude:" + position.coords.longitude);
+    }); 
+  }
+}
+
+</script>
+
+
+
+<template>
+<button @click="getLocation">Get Location</button>
+{{ latitude }}, {{ longitude }}
+</template>
+
+
+
+<style scoped>
+
+</style>
