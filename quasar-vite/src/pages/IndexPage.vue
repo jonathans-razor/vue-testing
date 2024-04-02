b0ba4ae0572aca7aec6b0ba3c94b05bb374ca7a6
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-02 14:34:48 -0400

    Apr-2-2024-2-34-47-PM

--- quasar-vite/src/pages/IndexPage.vue
+++ quasar-vite/src/pages/IndexPage.vue
@@ -4,7 +4,22 @@
     <q-btn label="Search" />
   </div>
   <br/>
-  <q-img alt="Venice Boat" src="../assets/venice boats.avif" width="500px"/>
+  <div>
+    <q-img alt="Venice Boat" src="../assets/venice boats.avif" width="500px"/>
+  </div>
+
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </modal>
+  </Teleport>
+
+
   </q-page>
 </template>
 
