78990fd42f66e13d3fa28a66df76bd0877d6d2b7
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-04 17:38:15 -0400

    Apr-4-2024-5-38-15-PM

--- shar/app/routes/_index.tsx
+++ shar/app/routes/_index.tsx
@@ -14,13 +14,29 @@ import {
   AccordionTrigger,
 } from "~/components/ui/accordion";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+import { Button } from "~/components/ui/button";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h2>Shadcn UI Samples</h2>
-      <h3>
-      Accordian
+
+      <h3 style={{ color: "red" }}>
+        Had an issue getting the TailwindCss styling to work here.
       </h3>
+
+      <br />
+
+      <h3>Accordian</h3>
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger>Is it accessible?</AccordionTrigger>
@@ -29,6 +45,29 @@ export default function Index() {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+
+      <br />
+
+      <h3>Card</h3>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
+      <br />
+
+      <h3>Button</h3>
+
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
