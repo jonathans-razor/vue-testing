09f1c252389c720fff1f0d4e5d3b45bfd61c4ad9
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-05 16:17:42 -0400

    Apr-5-2024-4-17-42-PM

--- sha3/src/App.vue
+++ sha3/src/App.vue
@@ -1,5 +1,60 @@
 <script setup lang="ts">
 import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+import {
   Accordion,
   AccordionContent,
   AccordionItem,
@@ -13,7 +68,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from '@/components/ui/tooltip'
+} from "@/components/ui/tooltip";
 
 const defaultValue = "item-1";
 
@@ -38,10 +93,10 @@ const accordionItems = [
 </script>
 
 <template>
-  <div style="margin-left: 0.5in">
-    <br>
+  <div style="margin-left: 0.5in;margin-right: 0.5in">
+    <br />
     <i class="text-purple-900"> Accordion </i>
-    <hr>
+    <hr />
     <Accordion
       type="single"
       class="w-full"
@@ -60,9 +115,9 @@ const accordionItems = [
       </AccordionItem>
     </Accordion>
 
-    <br>
+    <br />
     <i class="text-purple-900"> Button </i>
-    <hr>
+    <hr />
     <br />
     <div>
       <Button>Click me</Button>
@@ -70,7 +125,7 @@ const accordionItems = [
 
     <br />
     <i class="text-purple-900"> Checkbox </i>
-    <hr>
+    <hr />
     <br />
     <div class="flex items-center space-x-2">
       <Checkbox id="terms" />
@@ -85,30 +140,58 @@ const accordionItems = [
     <br />
     <br />
     <i class="text-purple-900"> Label </i>
-    <hr>
+    <hr />
     <br />
     <div>
       <div class="flex items-center space-x-2">
         <Label for="terms">Here is a label.</Label>
       </div>
     </div>
-    
+
     <br />
     <br />
     <i class="text-purple-900"> Tooltip </i>
-    <hr>
+    <hr />
     <br />
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger as-child>
-          <Button variant="outline">
-            Hover
-          </Button>
+          <Button variant="outline"> Hover </Button>
         </TooltipTrigger>
         <TooltipContent>
           <p>Add to library</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
+
+    <br />
+    <br />
+    <i class="text-purple-900"> Table </i>
+    <hr />
+    <br />
+
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-[100px]"> Invoice </TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead class="text-right"> Amount </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="invoice in invoices" :key="invoice.invoice">
+          <TableCell class="font-medium">
+            {{ invoice.invoice }}
+          </TableCell>
+          <TableCell>{{ invoice.paymentStatus }}</TableCell>
+          <TableCell>{{ invoice.paymentMethod }}</TableCell>
+          <TableCell class="text-right">
+            {{ invoice.totalAmount }}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   </div>
 </template>
