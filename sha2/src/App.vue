8dfc44fd0c71640d98a186397a5fcaa885bbe2e5
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-05 14:33:18 -0400

    Apr-5-2024-2-33-18-PM

--- sha2/src/App.vue
+++ sha2/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+import { Button } from '@/components/ui/button'
 </script>
 
 
@@ -7,6 +7,8 @@
 <template>
 <div class="container py-8">
   lorem ipsum
+  <Button>Button yeah</Button>
+  lorem ipsum2
 </div>
 
 
