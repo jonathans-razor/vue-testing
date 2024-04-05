aac91cea98dc6f97e6ba41b7c817ff55b7dde155
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-05 15:24:49 -0400

    Apr-5-2024-3-24-48-PM

--- sha3/src/App.vue
+++ sha3/src/App.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 </script>
 
-
-
 <template>
-  <i>
-    Button
-  </i>
-  <br>
-  <br>
+  <i> Accordion </i>
+  <Accordion type="single" collapsible>
+    <AccordionItem value="item-1">
+      <AccordionTrigger>Is it accessible?</AccordionTrigger>
+      <AccordionContent>
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+  <br />
+
+
+  <i> Button </i>
+  <br />
+  <br />
   <div>
     <Button>Click me</Button>
   </div>
