67a959bd21f9c3135d2fcb981cbb861991a9ca82
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-05 15:50:06 -0400

    Apr-5-2024-3-50-06-PM

--- sha3/src/App.vue
+++ sha3/src/App.vue
@@ -6,25 +6,76 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+
+const defaultValue = "item-1";
+
+const accordionItems = [
+  {
+    value: "item-1",
+    title: "Is it accessible?",
+    content: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    value: "item-2",
+    title: "Is it unstyled?",
+    content:
+      "Yes. It's unstyled by default, giving you freedom over the look and feel.",
+  },
+  {
+    value: "item-3",
+    title: "Can it be animated?",
+    content: "Yes! You can use the transition prop to configure the animation.",
+  },
+];
 </script>
 
 <template>
   <i> Accordion </i>
-  <Accordion type="single" collapsible>
-    <AccordionItem value="item-1">
-      <AccordionTrigger>Is it accessible?</AccordionTrigger>
+  <Accordion
+    type="single"
+    class="w-full"
+    collapsible
+    :default-value="defaultValue"
+  >
+    <AccordionItem
+      v-for="item in accordionItems"
+      :key="item.value"
+      :value="item.value"
+    >
+      <AccordionTrigger>{{ item.title }}</AccordionTrigger>
       <AccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
+        {{ item.content }}
       </AccordionContent>
     </AccordionItem>
   </Accordion>
-  <br />
 
-
+  <br>
   <i> Button </i>
-  <br />
   <br />
   <div>
     <Button>Click me</Button>
+  </div>
+
+  <br />
+  <i class="text-red-500"> Checkbox </i>
+  <br />
+  <div class="flex items-center space-x-2">
+    <Checkbox id="terms" />
+    <label
+      for="terms"
+      class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+    >
+      Accept terms and conditions
+    </label>
+  </div>
+
+  <br />
+  <i> Label </i>
+  <div>
+    <div class="flex items-center space-x-2">
+      <Label for="terms">Here is a label.</Label>
+    </div>
   </div>
 </template>
