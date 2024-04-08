3149f20170e635279f29597b00b352387371fb75
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-08 15:44:54 -0400

    Apr-8-2024-3-44-54-PM

--- quasar-vite/src/pages/IndexPage.vue
+++ quasar-vite/src/pages/IndexPage.vue
@@ -16,8 +16,8 @@ defineComponent({
       <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
 
       <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
+        <div class="text-h6">Time Has No Beginning And the Big Bang is Wrong</div>
+        <div class="text-subtitle2">by Roger Penrose</div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
