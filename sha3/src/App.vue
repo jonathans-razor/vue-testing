87c80a66a5a42677b7f2cd3624cb351e15aab272
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-08 11:48:58 -0400

    Apr-8-2024-11-48-58-AM

--- sha3/src/App.vue
+++ sha3/src/App.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-import { toast } from 'vue-sonner'
+
+import { useColorMode } from "@vueuse/core";
+import { Icon } from "@iconify/vue";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const mode = useColorMode()
+
+import { toast } from "vue-sonner";
 
 import {
   Card,
@@ -34,8 +46,6 @@ import {
 
 import { useToast } from "@/components/ui/toast/use-toast";
 import { Button } from "@/components/ui/button";
-
-const { toast } = useToast();
 
 import {
   Table,
@@ -127,6 +137,8 @@ const accordionItems = [
     content: "Yes! You can use the transition prop to configure the animation.",
   },
 ];
+const { toast } = useToast();
+
 </script>
 
 <template>
@@ -260,6 +272,51 @@ const accordionItems = [
     <br />
     <br />
     <br />
+    <i class="text-purple-900"> Color Mode </i>
+    <hr />
+    <br />
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button variant="outline">
+          <Icon
+            icon="radix-icons:moon"
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Icon
+            icon="radix-icons:sun"
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+          <span class="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
+        <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
+        <DropdownMenuItem @click="mode = 'auto'"> System </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
+    <br />
+    <br />
+    <br />
+    <i class="text-purple-900"> Dropdown Menu </i>
+    <hr />
+    <br />
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
+    <br />
+    <br />
+    <br />
     <i class="text-purple-900"> Label </i>
     <hr />
     <br />
@@ -292,7 +349,6 @@ const accordionItems = [
       </SelectContent>
     </Select>
 
-
     <br />
     <br />
     <br />
@@ -300,20 +356,22 @@ const accordionItems = [
     <hr />
     <br />
 
-      <Button
-    variant="outline" @click="() => {
-      toast('Event has been created', {
-        description: 'Sunday, December 03, 2023 at 9:00 AM',
-        action: {
-          label: 'Undo',
-          onClick: () => console.log('Undo'),
-        },
-      })
-    }"
-  >
-    Add to calendar
-  </Button>
-
+    <Button
+      variant="outline"
+      @click="
+        () => {
+          toast('Event has been created', {
+            description: 'Sunday, December 03, 2023 at 9:00 AM',
+            action: {
+              label: 'Undo',
+              onClick: () => console.log('Undo'),
+            },
+          });
+        }
+      "
+    >
+      Add to calendar
+    </Button>
 
     <br />
     <br />
