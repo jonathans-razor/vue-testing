bb362dfca4569bd5676cdb0226c21a0b8294dbfc
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-08 11:08:50 -0400

    Apr-8-2024-11-08-50-AM

--- sha3/src/App.vue
+++ sha3/src/App.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 import {
   AlertDialog,
@@ -10,7 +18,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from '@/components/ui/alert-dialog'
+} from "@/components/ui/alert-dialog";
 
 import {
   Select,
@@ -20,7 +28,7 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
+} from "@/components/ui/select";
 
 import { useToast } from "@/components/ui/toast/use-toast";
 import { Button } from "@/components/ui/button";
@@ -120,6 +128,9 @@ const accordionItems = [
 </script>
 
 <template>
+  <br />
+  <br />
+  <br />
   <div style="margin-left: 0.5in; margin-right: 0.5in; margin-bottom: 1in">
     <br />
     <i class="text-purple-900"> Accordion </i>
@@ -143,22 +154,93 @@ const accordionItems = [
     </Accordion>
 
     <br />
+    <br />
+    <i class="text-purple-900"> Alert Dialog </i>
+    <hr />
+    <br />
+
+    <AlertDialog>
+      <AlertDialogTrigger as-child>
+        <Button variant="outline"> Show Dialog </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. Are you sure you want to watch the
+            eclipse?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
+    <br />
+    <br />
+    <br />
     <i class="text-purple-900"> Button </i>
     <hr />
     <br />
     <div>
-    <Button
-      variant="outline"
-      @click="
-        () => {
-          console.log('Button clicked');
-        }
-      "
-    >
-      Click Me
-    </Button>
+      <Button
+        variant="outline"
+        @click="
+          () => {
+            console.log('Button clicked');
+          }
+        "
+      >
+        Click Me
+      </Button>
     </div>
 
+    <br />
+    <br />
+    <br />
+    <i class="text-purple-900"> Card </i>
+    <hr />
+    <br />
+
+    <Card class="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div class="grid items-center w-full gap-4">
+            <div class="flex flex-col space-y-1.5">
+              <Label for="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div class="flex flex-col space-y-1.5">
+              <Label for="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="nuxt"> Nuxt.js </SelectItem>
+                  <SelectItem value="next"> Next.js </SelectItem>
+                  <SelectItem value="sveltekit"> SvelteKit </SelectItem>
+                  <SelectItem value="astro"> Astro </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter class="flex justify-between px-6 pb-6">
+        <Button variant="outline"> Cancel </Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+
+    <br />
+    <br />
     <br />
     <i class="text-purple-900"> Checkbox </i>
     <hr />
@@ -175,6 +257,7 @@ const accordionItems = [
 
     <br />
     <br />
+    <br />
     <i class="text-purple-900"> Label </i>
     <hr />
     <br />
@@ -186,20 +269,28 @@ const accordionItems = [
 
     <br />
     <br />
-    <i class="text-purple-900"> Tooltip </i>
+    <br />
+    <i class="text-purple-900"> Select </i>
     <hr />
     <br />
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger as-child>
-          <Button variant="outline"> Hover </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Add to library</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
 
+    <Select>
+      <SelectTrigger class="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple"> Apple </SelectItem>
+          <SelectItem value="banana"> Banana </SelectItem>
+          <SelectItem value="blueberry"> Blueberry </SelectItem>
+          <SelectItem value="grapes"> Grapes </SelectItem>
+          <SelectItem value="pineapple"> Pineapple </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+
+    <br />
     <br />
     <br />
     <i class="text-purple-900"> Table </i>
@@ -232,6 +323,7 @@ const accordionItems = [
 
     <br />
     <br />
+    <br />
     <i class="text-purple-900"> Toast </i>
     <hr />
     <br />
@@ -252,64 +344,21 @@ const accordionItems = [
       Add to calendar
     </Button>
 
-        <br />
     <br />
-    <i class="text-purple-900"> Select </i>
+    <br />
+    <br />
+    <i class="text-purple-900"> Tooltip </i>
     <hr />
     <br />
-
-
-  <Select>
-    <SelectTrigger class="w-[180px]">
-      <SelectValue placeholder="Select a fruit" />
-    </SelectTrigger>
-    <SelectContent>
-      <SelectGroup>
-        <SelectLabel>Fruits</SelectLabel>
-        <SelectItem value="apple">
-          Apple
-        </SelectItem>
-        <SelectItem value="banana">
-          Banana
-        </SelectItem>
-        <SelectItem value="blueberry">
-          Blueberry
-        </SelectItem>
-        <SelectItem value="grapes">
-          Grapes
-        </SelectItem>
-        <SelectItem value="pineapple">
-          Pineapple
-        </SelectItem>
-      </SelectGroup>
-    </SelectContent>
-  </Select>
-
-    <br />
-    <br />
-    <i class="text-purple-900"> Alert Dialog </i>
-    <hr />
-    <br />
-  
-    <AlertDialog>
-    <AlertDialogTrigger as-child>
-      <Button variant="outline">
-        Show Dialog
-      </Button>
-    </AlertDialogTrigger>
-    <AlertDialogContent>
-      <AlertDialogHeader>
-        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-        <AlertDialogDescription>
-          This action cannot be undone. Are you sure you want to watch the eclipse?
-        </AlertDialogDescription>
-      </AlertDialogHeader>
-      <AlertDialogFooter>
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction>Continue</AlertDialogAction>
-      </AlertDialogFooter>
-    </AlertDialogContent>
-  </AlertDialog>
-
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger as-child>
+          <Button variant="outline"> Hover </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   </div>
 </template>
