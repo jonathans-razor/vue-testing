3da4d2e780775946cf9e1d42567b31382f0bf946
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-08 16:02:59 -0400

    Apr-8-2024-4-02-59-PM

--- quasar-vite/src/pages/IndexPage.vue
+++ quasar-vite/src/pages/IndexPage.vue
@@ -11,31 +11,38 @@ defineComponent({
 </script>
 
 <template>
+
+
+
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-parallax
+        src="https://cdn.quasar.dev/img/parallax1.jpg"
+        :height="150"
+      />
+
+      <q-card-section>
+        <div class="text-h6">Card With Parallax</div>
+      </q-card-section>
+    </q-card>
+  </div>
+
+
   <div class="q-pa-md">
     <q-card class="my-card">
       <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
 
       <q-card-section>
         <div class="text-h6">
-          Time Has No Beginning And the Big Bang is Wrong
+          Card With Video
         </div>
-        <div class="text-subtitle2">by Roger Penrose</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </q-card-section>
     </q-card>
   </div>
 
   <div class="q-pa-md">
-    <br />
     <q-btn id="show-modal" @click="showModal = true">Show Modal</q-btn>
-  </div>
-  <br />
-  <div class="q-pa-md">
-    <q-img alt="Venice Boat" src="../assets/venice boats.avif" width="150px" />
   </div>
 
   <Teleport to="body">
