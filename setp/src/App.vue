ec838465e23554775d1660fd6de9be7221f5b781
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-08 14:39:26 -0400

    Apr-8-2024-2-39-26-PM

--- setp/src/App.vue
+++ setp/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+import { Button } from '@/components/ui/button'
 </script>
 
 
@@ -7,5 +7,25 @@
 <template>
   <div style="margin-left: 0.5in; margin-right: 0.5in; margin-top: .2in; margin-bottom: 1in">
   Hello World    
+
+   <br />
+    <br />
+    <br />
+    <i class="text-purple-900"> Button </i>
+    <hr />
+    <br />
+    <div>
+      <Button
+        variant="outline"
+        @click="
+          () => {
+            console.log('Button clicked');
+          }
+        "
+      >
+        Click Me
+      </Button>
+
+    </div>
   </div>
 </template>
