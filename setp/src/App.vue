a1efdca8a815cc4d7d01f1ae127f065a6fc908a8
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-08 14:34:23 -0400

    Apr-8-2024-2-34-23-PM

---
+++ setp/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+
+
+<template>
+  <div style="margin-left: 0.5in; margin-right: 0.5in; margin-top: .2in; margin-bottom: 1in">
+  Hello World    
+  </div>
+</template>
