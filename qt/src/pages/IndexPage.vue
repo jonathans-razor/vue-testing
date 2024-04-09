4cad0b71845abcf564d913480ff4562bf9dfbd1e
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-09 16:53:52 -0400

    Apr-9-2024-4-53-51-PM

--- qt/src/pages/IndexPage.vue
+++ qt/src/pages/IndexPage.vue
@@ -1,4 +1,67 @@
 <template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn label="Alert" color="primary" @click="alert = true" />
+    <q-btn label="Confirm" color="primary" @click="confirm = true" />
+    <q-btn label="Prompt" color="primary" @click="prompt = true" />
+
+    <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
+          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
+          minima, porro labore.
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+          <span class="q-ml-sm"
+            >You are currently not connected to any network.</span
+          >
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Your address</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input
+            dense
+            v-model="address"
+            autofocus
+            @keyup.enter="prompt = false"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Add address" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+
   <div class="q-pa-md">
     <q-ajax-bar
       ref="bar"
@@ -9,6 +72,9 @@
     />
 
     <q-btn color="primary" label="Ajax Bar Trigger" @click="trigger" />
+    <br />
+    <br />
+    <q-btn color="primary" label="Toggle Dark Mode" @click="toggle" />
   </div>
 
   <br />
@@ -28,11 +94,28 @@
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
 import { defineComponent, ref } from 'vue';
+import { useQuasar } from 'quasar';
 
 export default defineComponent({
   name: 'IndexPage',
   components: { ExampleComponent },
   setup() {
+    const $q = useQuasar();
+
+    // get status
+    console.log('1. is dark mode active:' + $q.dark.isActive); // true, false
+
+    // get configured status
+    console.log($q.dark.mode); // "auto", true, false
+
+    // set status
+    $q.dark.set(true); // or false or "auto"
+
+    // toggle
+    //$q.dark.toggle();
+
+    console.log('2. is dark mode active:' + $q.dark.isActive); // true, false
+
     const bar = ref(null);
 
     // we manually trigger it (this is not needed if we
@@ -47,6 +130,10 @@ export default defineComponent({
           barRef.stop();
         }
       }, Math.random() * 3000 + 1000);
+    }
+
+    function toggle() {
+      $q.dark.set(!($q.dark.isActive));
     }
 
     const todos = ref<Todo[]>([
@@ -75,10 +162,15 @@ export default defineComponent({
       totalCount: 1200,
     });
     return {
+      alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+      address: ref(''),
       bar,
+      toggle,
       trigger,
       todos,
-      meta
+      meta,
     };
   },
 });
