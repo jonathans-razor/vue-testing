117d4c1908cc1e44daa0d81666acca5de54b9930
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-11 16:30:25 -0400

    Apr-11-2024-4-30-25-PM

--- fova/src/App.vue
+++ fova/src/App.vue
@@ -1,8 +1,49 @@
 
 <template>
-  <div></div>
+  <div class="root">
+    <h2>Create an Account</h2>
+    <p>
+      <input type="text" placeholder="Email" />
+    </p>
+    <p>
+      <input type="password" placeholder="Password" />
+    </p>
+    <p>
+      <input type="password" placeholder="Confirm Password" />
+    </p>
+    <button>Submit</button>
+  </div>
 </template>
 
 <script>
 
 </script>
+
+<style lang="css">
+.root {
+  width: 400px;
+  margin: 0 auto;
+  background-color: #fff;
+  padding: 30px;
+  margin-top: 100px;
+  border-radius: 20px;
+}
+
+input {
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #ddd;
+  font-size: 1em;
+  padding: 5px 0;
+  margin: 10px 0 5px 0;
+  width: 100%;
+}
+
+button {
+  background-color: #3498db;
+  padding: 10px 20px;
+  margin-top: 10px;
+  border: none;
+  color: white;
+}
+</style>
