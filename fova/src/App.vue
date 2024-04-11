77724258a806279bf7a0ae0b730fecd309dcc4d2
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-11 15:33:46 -0400

    Apr-11-2024-3-33-45-PM

--- fova/src/App.vue
+++ fova/src/App.vue
@@ -3,9 +3,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue' // "from '@vue/composition-api'" if you are using Vue <2.7
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup () {
+    const state = reactive({
+      firstName: '',
+      lastName: '',
+      contact: {
+        email: ''
+      }
+    })
+    const rules = {
+      firstName: { required }, // Matches state.firstName
+      lastName: { required }, // Matches state.lastName
+      contact: {
+        email: { required, email } // Matches state.contact.email
+      }
+    }
+
+    const v$ = useVuelidate(rules, state)
+
+    return { state, v$ }
+  }
 });
+
 </script>
