6292351bd56c034129497ec347a07dbd83feae82
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-19 15:12:01 -0400

    Apr-19-2024-3-12-01-PM

---
+++ http-axios/src/App-get.vue
@@ -0,0 +1,42 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div v-for="post in posts" :key="post.id">
+    <h2>
+      {{ post.id }}
+      {{ post.title }}
+    </h2>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.posts = response.data
+      })
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
