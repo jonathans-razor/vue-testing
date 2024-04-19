ff63963aef45fcd046c940e6ed01a683033d2303
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-19 14:50:41 -0400

    Apr-19-2024-2-50-40-PM

---
+++ http-axios/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => {
+        console.log(response.data.bpi)
+      })
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
