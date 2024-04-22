3f766eeff12d205b326e546598885f7f701e2920
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-22 16:34:54 -0400

    Apr-22-2024-4-34-54-PM

---
+++ vai/src/input-event.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Your name: {{ name }}</p>
+  <input type="text" @input="getInput">
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    getInput() {
+      // get value of input on event object
+      // and assign it to 'name' data property
+      this.name = event.target.value
+    }
+  }
+}
+</script>
