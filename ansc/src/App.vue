3efbc65c0f32bd07b5e5b99c7da324a8d0c697c9
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-22 15:29:46 -0400

    Apr-22-2024-3-29-46-PM

--- ansc/src/App.vue
+++ ansc/src/App.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts">
+<script setup >
+import DataTable from './components/DataTable.vue';
+
+const items = [
+  {
+    id: 1,
+    name: 'John Doe'
+  },
+  {
+    id: 2,
+    name: 'Jane Doe'
+  }
+];
 </script>
 
+
 <template>
+  <div class="p-8 bg-gray-100 min-h-screen">
+    <DataTable :items="items" />
+  </div>
 </template>
---
+++ ansc/src/components/DataTable.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <table>
+    <tr v-for="item in items" :key="item.id">
+      <td> {{ item.id }}</td>
+    </tr>
+  </table>
+</template>
