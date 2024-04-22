d5129bddc0f2394e291e9e9ff25e9f8d2bb75e79
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-22 15:54:07 -0400

    Apr-22-2024-3-54-07-PM

---
+++ vai/src/change-name.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ name }}</p>
+  <button v-on:click="name = 'The Cooler Daniel'">Change name</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Daniel'
+    }
+  }
+}
+</script>
