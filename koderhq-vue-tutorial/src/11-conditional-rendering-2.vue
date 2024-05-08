ffe33c141f2a8482f77ad33e93f95e3b0f09f97a
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-05-08 15:43:46 -0400

    May-8-2024-3-43-46-PM

---
+++ koderhq-vue-tutorial/src/11-conditional-rendering-2.vue
@@ -0,0 +1,16 @@
+<template>
+  <p v-if="num === 0">The number is 0</p>
+  <p v-else-if="num > 0">The number is positive</p>
+  <p v-else-if="num < 0">The number is negative</p>
+  <p v-else>The number is a lie</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 'x'
+    }
+  }
+}
+</script>
---
+++ koderhq-vue-tutorial/src/11-conditional-rendering.vue
@@ -0,0 +1,13 @@
+<template>
+  <p v-if="num === 0">The number is 0</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 0
+    }
+  }
+}
+</script>
