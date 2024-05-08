bd61cabd3beac845fa95a228f7d02f73d72356c3
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-05-08 13:31:38 -0400

    May-8-2024-1-31-37-PM

---
+++ vai/src/10-watcher.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Counter: {{ count }}</p>
+
+  <button @click="count++">Increment</button>
+  <button @click="count--">Decrement</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  watch: {
+    count(newValue) {
+      if (newValue === 5) alert('Value reached 5')
+    }
+  }
+}
+</script>
