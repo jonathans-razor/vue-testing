e8449e9251e2797c1fb291a785f20fcde7ad19be
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-05-08 12:50:15 -0400

    May-8-2024-12-50-15-PM

---
+++ vai/src/form-handling-basics.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Name: {{ formData.firstName }} {{ formData.lastName }}</p>
+  <form @submit.prevent="submitForm">
+    <p>
+      <label for="firstName">First Name:</label>
+      <input id="firstName" type="text" v-model.lazy.trim="formData.firstName">
+    </p>
+    <p>
+      <label for="lastName">Last Name:</label>
+      <input id="lastName" type="text" v-model.lazy.trim="formData.lastName">
+    </p>
+    <button>Submit Form</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        firstName: '',
+        lastName: ''
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('Form Data', this.formData)
+    }
+  }
+}
+</script>
