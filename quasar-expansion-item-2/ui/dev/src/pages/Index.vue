4249a25aba9f524e4ab31261ab854e17cdf3635a
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2025-05-21 16:12:10 -0400

    May-21-2025-4-12-09-PM

--- quasar-expansion-item-2/ui/dev/src/pages/Index.vue
+++ quasar-expansion-item-2/ui/dev/src/pages/Index.vue
@@ -1,40 +1,60 @@
 <template>
-  <q-page padding class="row justify-center">
-    <q-list dense class="list">
-      <div class="text-h4 q-mb-md">Test pages</div>
-      <q-item
-        v-for="page in pages"
-        :key="page.path"
-        :to="page.path"
+  <q-page class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-expansion-item
+        icon="info"
+        label="What is Quasar?"
+        expand-separator
+        v-model="expanded"
       >
-        <q-item-section avatar>
-          <q-icon name="pages" />
-        </q-item-section>
-        <q-item-section>
-          {{ page.title }}
-        </q-item-section>
-        <q-item-section side>
-          <q-icon name="chevron_right" />
-        </q-item-section>
-      </q-item>
-    </q-list>
+        <div>
+          Quasar is a high-performance Vue.js framework that allows you to build responsive
+          websites, PWAs, mobile apps, and Electron apps with a single codebase.
+        </div>
+      </q-expansion-item>
+
+      <q-expansion-item
+        icon="build"
+        label="How to install Quasar?"
+        expand-separator
+        v-model="expanded2"
+      >
+        <div>
+          You can install Quasar via npm or yarn:
+          <pre><code>npm install -g @quasar/cli
+quasar create my-app
+cd my-app
+quasar dev</code></pre>
+        </div>
+      </q-expansion-item>
+
+      <q-expansion-item
+        icon="code"
+        label="Sample Code"
+        expand-separator
+        v-model="expanded3"
+      >
+        <div>
+          Here's a simple example of using <code>&lt;q-expansion-item&gt;</code>:
+          <pre><code>
+&lt;q-expansion-item label="Example"&gt;
+  &lt;div&gt;Content inside expansion item&lt;/div&gt;
+&lt;/q-expansion-item&gt;
+          </code></pre>
+        </div>
+      </q-expansion-item>
+    </div>
   </q-page>
 </template>
 
 <script>
-import pages from '../router/pages'
-
 export default {
-  setup () {
+  data() {
     return {
-      pages
+      expanded: false,
+      expanded2: false,
+      expanded3: false,
     }
   }
 }
 </script>
-
-<style lang="sass" scoped>
-.list
-  width: 700px
-  max-width: 100%
-</style>
