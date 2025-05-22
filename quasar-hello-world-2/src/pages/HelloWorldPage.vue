b1c2980bbd41fd4c11986f7395818fc4921e8fea
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2025-05-22 15:35:26 -0400

    May-22-2025-3-35-25-PM

---
+++ quasar-hello-world-2/src/pages/HelloWorldPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Hello World</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #2c3e50;
+}
+</style>
